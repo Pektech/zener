83a1af2c623090d350b85e778a7734d358594188
Ric Leddy <14008446+Pektech@users.noreply.github.com>
2018-10-23 10:02:56 -0400

    still setting up

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,21 @@
-from flask import Flask, send_from_directory
+from flask import Flask
 from flask_ask import Ask
 
 app = Flask(__name__)
-ask = Ask(app, '/')
+ask = Ask(app, "/")
 
 import logging
 
-logging.getLogger('flask_ask').setLevel(logging.DEBUG)
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 
 from app import alexa
 
-@app.route('/pek')
+
+@app.route("/pek")
 def hello_world():
-    return 'Hello World!'
+    return "Hello Pek and Monk"
 
 
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
