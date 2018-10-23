6ce4e437801e34caf183d19e15eeb9c7fc1a4d4a
Ric Leddy <14008446+Pektech@users.noreply.github.com>
2018-10-23 13:02:27 -0400

    added game play, intents for ready and playing game

--- app/alexa.py
+++ app/alexa.py
@@ -1,5 +1,6 @@
 from .cards import cards, shuffled_cards
 from app import ask
+from .mechanics import DECK
 
 from flask_ask import statement, question
 from flask_ask import session as ask_session, request as ask_request, context
@@ -9,9 +10,47 @@ from flask import render_template
 @ask.launch
 def start_skill():
     output = render_template('welcome')
+    ask_session.attributes['GAME_RUNNING'] = 0
+    ask_session.attributes['SCORE'] = 0
+    ask_session.attributes['DECK'] = DECK
+    ask_session.attributes['CARD_COUNT'] = 0
     ask_session.attributes['last_speech'] = output
     return question(output).reprompt(ask_session.attributes['last_speech'])
 
+@ask.intent('ready')
+def start_game():
+    if ask_session.attributes['GAME_RUNNING'] == 1:
+        output = render_template('playing')
+        return question(output)
+    else:
+        output = render_template('ready')
+        return question(output)
+
+
+
+
+
+@ask.intent('zener')
+def play_game(guess):
+    ask_session.attributes['GAME_RUNNING'] = 1
+    score = ask_session.attributes['SCORE']
+    game_deck = ask_session.attributes['DECK']
+    card_count = ask_session.attributes['CARD_COUNT']
+    while card_count < 25:
+        if guess == 'ready':
+            output = render_template('ready')
+            return question(output)
+        output = render_template('game')
+
+        print(game_deck[card_count], guess, "score = ", score)
+        if guess == game_deck[card_count]:
+            score += 1
+        card_count += 1
+        ask_session.attributes['CARD_COUNT'] = card_count
+        ask_session.attributes['SCORE'] = score
+        return question(output)
+    output = render_template('score', score=score)
+    return statement(output)
 
 
 
--- app/mechanics.py
+++ app/mechanics.py
@@ -7,13 +7,14 @@ CARD_COUNT = 0
 DECK = shuffled_cards(cards)
 SCORE = 0
 
+"""Test mechanics of game"""
+# while CARD_COUNT < 25:
+#     choosen_card = DECK[CARD_COUNT]
+#     guessed_card = input("choose card")
+#
+#     if guessed_card == choosen_card:
+#         SCORE += 1
+#     CARD_COUNT += 1
+#
+# print(f'You scored {SCORE}')
 
-while CARD_COUNT < 25:
-    choosen_card = DECK[CARD_COUNT]
-    guessed_card = input("choose card")
-
-    if guessed_card == choosen_card:
-        SCORE += 1
-    CARD_COUNT += 1
-
-print(f'You scored {SCORE}')
