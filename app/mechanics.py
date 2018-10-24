c7e7d7506cc365cb7241d4c98f3e141461c36ac2
Ric Leddy <14008446+Pektech@users.noreply.github.com>
2018-10-23 23:25:48 -0400

    random dialogue

--- app/mechanics.py
+++ app/mechanics.py
@@ -6,6 +6,7 @@ from app.cards import cards, shuffled_cards
 CARD_COUNT = 0
 DECK = shuffled_cards(cards)
 SCORE = 0
+DECK_SIZE = 25
 
 """Test mechanics of game"""
 # while CARD_COUNT < 25:
@@ -23,7 +24,7 @@ def final_score(score):
         return f"Wow either you are really unlucky or have " \
                f"some sort of anti-clairvoyance skill. You scored {score} "
     elif 3 <= score <= 10 :
-        return f" Not a bad score but it's what the average person score. " \
-               f"Your final score was {score}"
+        return f" Not a bad score but the average person scores between 3 and 8. " \
+               f"Your final score was {score}. yeah pretty average"
     else:
         return f"Wow {score} that is a really high score. Maybe you should play the lottery"
