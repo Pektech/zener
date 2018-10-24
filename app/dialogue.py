2a5785ece6c7e8502d7abe2e8e145660b4e2ba5d
Ric Leddy <14008446+Pektech@users.noreply.github.com>
2018-10-23 23:54:19 -0400

    polish 2.0

--- app/dialogue.py
+++ app/dialogue.py
@@ -3,7 +3,7 @@ from random import choice
 choose_card = ['what is the next card', 'Next card please',
                'Focus, see the next card, be the next card, say the next card',
                'And the next card is ?', 'next card',
-               'let your mind go blank, well not that blank. What card is next?',
+               'let your mind go blank. Well not that blank. What card is next?',
                'Concentrate and say whats next',
                "You've got an idea forming in your head. Its a card called?",
                "Name that card" ]
